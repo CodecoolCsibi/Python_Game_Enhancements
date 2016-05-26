59bc8495fa930025f1e7a29b5a13e3f8ac38243b
CsibiJani <jozsef.berko.ifj@gmail.com>
2016-05-26 10:10:16 +0200

    Highscores function is now 100%.There are no global variables anymore.

--- NewSnake.py
+++ NewSnake.py
@@ -1,43 +1,19 @@
 import curses
 import random
 import time
-from collections import OrderedDict
 
-headC = "^"
-poison_ed = False
-win = curses.initscr()
-border = win.getmaxyx()
-title = "SNAKE by Csibi & Dzsoni"
-speed = 0.10
-head = [int(border[0]/2),int(border[1]/2)]
-body = [head[:]]*3
-apple = [random.randint(2, border[0]-2), random.randint(2, border[1]-2)]
-pois = [random.randint(2, border[0]-2), random.randint(2, border[1]-2)]
-q = 0
-points = 0
-direct = 8
-win.nodelay(1)
-last = []
-def declarations():
-    win = curses.initscr()
-    curses.start_color()
-    curses.noecho()
-    curses.curs_set(0)
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_GREEN)
-    win.bkgd(" ", curses.color_pair(1))
 
-declarations()
+def bodygrow(body, head, win):
 
-def bodygrow():
-    global head, body, last
     for z in range(len(body)-1, 0, -1):
         body[z] = body[z-1]
-        win.addch(body[z][0],body[z][1] , "o", curses.color_pair(2))
+        win.addch(body[z][0], body[z][1], "o", curses.color_pair(2))
     body[0] = head[:]
+    return body
 
-def food():
-    global q, head, apple, points, body, speed, pois,poison_ed
+
+def food(q, head, apple, points, body, speed, pois, poison_ed, win, border):
+
     win.addch(apple[0], apple[1], "B", curses.color_pair(2))
     if head[0] == apple[0] and head[1] == apple[1]:
         points += 1
@@ -51,10 +27,11 @@ def food():
         win.addch(pois[0], pois[1], "ß", curses.color_pair(2))
         win.refresh()
     win.addstr(0, 0, "Points:" + str(points), curses.color_pair(2))
-    win.addstr(2, 0, "Poison_ed:" + str(poison_ed), curses.color_pair(2))
+    return speed, body, apple, pois, points
 
-def poison():
-    global q, head, pois, points, poison_ed, pois_rounds
+
+def poison(q, head, apple, points, body, speed, pois, poison_ed, win, border):
+
     win.addch(pois[0], pois[1], "ß", curses.color_pair(2))
     win.refresh()
     if head[0] == pois[0] and head[1] == pois[1]:
@@ -63,11 +40,11 @@ def poison():
         pois = [random.randint(2, border[0]-2), random.randint(2, border[1]-2)]
         win.addch(pois[0], pois[1], "ß", curses.color_pair(2))
         win.refresh()
+    return poison_ed, pois
 
 
-def hitWall():
-    global head, q, border
-    border = win.getmaxyx()
+def hitWall(win, head, border, q):
+
     if head[0] == border[0]-1:
         q = ord("q")
     elif head[0] == 0:
@@ -76,10 +53,10 @@ def hitWall():
         q = ord("q")
     elif head[1] == 0:
         q = ord("q")
+    return q
 
 
-def turn():
-    global headC, head, q, border, direct
+def turn(q, head, border, headC, direct):
 
     if q == ord("8") and head[0] > 0:
         headC = "^"
@@ -93,10 +70,10 @@ def turn():
     elif q == ord("4") and head[1] > 0:
         headC = "<"
         direct = 4
+    return headC, direct
 
 
-def turn_two():
-    global headC, head, q, border, direct
+def turn_confused(q, direct, head, border):
     headC = "@"
     if q == ord("8") and head[0] > 0:
         direct = 5
@@ -106,14 +83,15 @@ def turn_two():
         direct = 4
     elif q == ord("4") and head[1] > 0:
         direct = 6
+    return headC, direct
 
 
-def forward():
-    global q, head, headC, direct, speed
-    if direct ==  4:
+def forward(head, headC, direct, speed, q):
+
+    if direct == 4:
         if direct != 6:
             head[1] -= 1
-    elif direct ==  6:
+    elif direct == 6:
         if direct != 4:
             head[1] += 1
     elif direct == 5:
@@ -123,10 +101,11 @@ def forward():
         if direct != 5:
             head[0] -= 1
     time.sleep(speed)
+    return head
 
 
-def start():
-    global border
+def start(border, win):
+
     win.clear()
     mess1 = "   #######    ###     ##       ####       ##     ##  ###########"
     mess2 = " ##      ##   ####    ##      ##  ##      ##    ##   ##         "
@@ -140,7 +119,7 @@ def start():
     mess10 = " ##      ##   ##    ####  ##          ##  ##    ##   ##         "
     mess11 = "  ########    ##     ###  ##          ##  ##     ##  ###########"
     mess12 = "Please choose a difficulty: 1.Easy      2.Medium      3.Hard"
-    snakeText = [mess1, mess2, mess3,mess4,mess5,mess6,mess7,mess8,mess9,mess10,mess11,mess12]
+    snakeText = [mess1, mess2, mess3, mess4, mess5, mess6, mess7, mess8, mess9, mess10, mess11, mess12]
     coordY = -5
     for z in snakeText:
         if z == mess12:
@@ -152,26 +131,10 @@ def start():
     win.refresh()
 
 
-def game():
-    global border, head, body, direct, q, points, speed, poison_ed
-    win = curses.initscr()
-    border = win.getmaxyx()
-    head = [int(border[0]/2), int(border[1]/2)]
-    title = "SNAKE by Csibi & Dzsoni"
-    speed = 0.10
-    apple = [random.randint(2, border[0]-2), random.randint(2, border[1]-2)]
-    pois = [random.randint(2, border[0]-2), random.randint(2, border[1]-2)]
-    win.nodelay(1)
-    last = []
-    direct = 0
-    q = -1
-    points = 0
-    body = [head[:]]*3
-    pois_rounds = 70
-
+def difficulty(q, speed, win,border):
     while q == -1:
         q = win.getch()
-        start()
+        start(border, win)
         if q == ord("1"):
             speed = 0.15
         elif q == ord("2"):
@@ -180,35 +143,62 @@ def game():
             speed = 0.07
         else:
             q = -1
+    return q, speed
 
-    while q != ord("q"):
-        win.clear()
-        win.border(1)
-        if poison_ed is False:
-            turn()
+
+def high_score(poison_ed, score, win):
+    win.clear()
+    curses.endwin()
+    pos = 9
+    h_score = open("highscores.csv", "r")
+    score_list = list(h_score.readlines())
+    h_score.close()
+    for i in range(len(score_list)-1, -1, -1):
+        score_list[i] = score_list[i][:-1]
+        score_list[i] = score_list[i].split(",")
+        if score >= int(score_list[i][1]):
+            pos = i
+    if pos != 9:
+        name = input("\nGratulations! You are the %d. in the Highscores.Please enter your name:" % (pos+1))
+        for z in range(len(score_list)-1, pos, -1):
+            score_list[z][0] = str(score_list[z-1][0])
+            score_list[z][1] = int(score_list[z-1][1])
+
+        score_list[pos][1] = score
+        score_list[pos][0] = name
+        h_score = open("highscores.csv", "w")
+        h_score.writelines(score_list[n][0] + "," + str(score_list[n][1])+"\n" for n in range(len(score_list)))
+        h_score.close()
+
+    else:
+        print("The Higscores are:")
+    for i in range(len(score_list)):
+        print(str(i+1) + ". " + str(score_list[i][0]) + " : " + str(score_list[i][1]))
+    answer = input("\nDo you want to play again?(y/n):")
+    if answer == "y":
+        poison_ed = False
+        game()
+    else:
+        raise SystemExit
+    return poison_ed
+
+
+def confusion(win, q, poison_ed, pois_rounds, head, border, headC, direct):
+    if poison_ed is False:
+        headC, direct = turn(q, head, border, headC, direct)
+    else:
+        if pois_rounds > 0:
+            headC, direct = turn_confused(q, direct, head, border)
+            pois_rounds -= 1
+            win.addstr(1, 0, "Poisoned:" + str(pois_rounds), curses.color_pair(2))
         else:
-            if pois_rounds > 0:
-                turn_two()
-                pois_rounds -= 1
-                win.addstr(1, 0, "Poisoned:" + str(pois_rounds), curses.color_pair(2))
-            else:
-                poison_ed = False
-                pois_rounds = 70
-
-        win.addstr(0, int(border[1]/2)-int(len(title)/2), title, curses.A_BOLD)
-        food()
-        forward()
-        bodygrow()
-        win.addstr(head[0], head[1], headC, curses.color_pair(2))
-        win.refresh()
-        q = win.getch()
-        poison()
-        hitWall()
-    gameover()
+            poison_ed = False
+            pois_rounds = 70
+    return win, q, poison_ed, pois_rounds, direct, headC
 
 
-def gameover():
-    global q, headC
+def gameover(poison_ed, q, headc, border, points, win):
+
     headC = "^"
     win.clear()
     while q != ord("r"):
@@ -221,10 +211,10 @@ def gameover():
         message2 = "   ##      ##     ##    ##     ####     ####  ##          "
         message3 = "  ##             ##      ##    ##  ## ##  ##  ##          "
         message4 = " ##              ##########    ##   ###   ##  ########### "
-        message5 =" ##     ######  ##        ##   ##         ##  ########### "
-        message6 ="  ##        ##  ##        ##   ##         ##  ##          "
-        message7 ="   ##      ##  ##          ##  ##         ##  ##          "
-        message8 ="    ########   ##          ##  ##         ##  ########### "
+        message5 = " ##     ######  ##        ##   ##         ##  ########### "
+        message6 = "  ##        ##  ##        ##   ##         ##  ##          "
+        message7 = "   ##      ##  ##          ##  ##         ##  ##          "
+        message8 = "    ########   ##          ##  ##         ##  ########### "
         message9 = "                                                          "
         message10 = "     ########   ##          ##  ###########   #########   "
         message11 = "    ##      ##  ##          ##  ##            ##      ##  "
@@ -235,7 +225,7 @@ def gameover():
         message16 = "    ##      ##      ##  ##      ##            ##       ## "
         message17 = "     ########        ####       ###########   ##        ##"
 
-        GOText = [message1, message2, message3,message4,message5,message6,message7,message8,message9,message10,message11,message12,message13,message14,message15,message16,message17]
+        GOText = [message1, message2, message3, message4, message5, message6, message7, message8, message9, message10, message11, message12, message13, message14, message15, message16, message17]
         k = -8
         end_text = ("Press R restart H to see the Highscores, (or Q to quit)")
         for z in GOText:
@@ -249,37 +239,48 @@ def gameover():
             break
     win.clear()
     curses.endwin()
-    high_score(points)
-    game()
+    poison_ed = high_score(poison_ed, points, win)
+    return poison_ed, q, headc
 
 
-def high_score(score):
-    pos = 9
-    h_score = open("highscores.csv", "r")
-    score_list = list(h_score.readlines())
-    h_score.close()
-    for i in range(len(score_list)-1, -1, -1):
-        score_list[i] = score_list[i][:-1]
-        score_list[i] = score_list[i].split(",")
-        if score > int(score_list[i][1]):
-            pos = i
-    if pos != 9:
-        name = input("\nGratulations! You are the %d. in the Highscores.Please enter your name:" %(pos+1))
-        score_list[pos][0] = name
-        score_list[pos][1] = score
-        h_score = open("highscores.csv", "w")
-        h_score.writelines(score_list[n][0] +"," + str(score_list[n][1])+"\n" for n in range(len(score_list)))
-        h_score.close()
-
-    else:
-        print("The Higscores are:")
-    for i in range(len(score_list)):
-        print(str(i+1) + ". " + str(score_list[i][0]) + " : " + str(score_list[i][1]))
-    answer = input("\nDo you want to play again?(y/n):")
-    if answer == "y":
-        game()
-    else:
-        raise SystemExit
+def game():
+    headC = "^"
+    poison_ed = False
+    win = curses.initscr()
+    border = win.getmaxyx()
+    title = "SNAKE by Csibi & Dzsoni"
+    speed = 0.10
+    head = [int(border[0]/2), int(border[1]/2)]
+    body = [head[:]]*3
+    q = -1
+    points = 0
+    direct = 8
+    win.nodelay(1)
+    last = []
+    curses.start_color()
+    curses.noecho()
+    curses.curs_set(0)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_GREEN)
+    win.bkgd(" ", curses.color_pair(1))
+    apple = [random.randint(2, border[0]-2), random.randint(2, border[1]-2)]
+    pois = [random.randint(2, border[0]-2), random.randint(2, border[1]-2)]
+    pois_rounds = 70
+    q, speed = difficulty(q, speed, win, border)
+    while q != ord("q"):
+        win.clear()
+        win.border(1)
+        win, q, poison_ed, pois_rounds, direct, headC = confusion(win, q, poison_ed, pois_rounds, head, border, headC, direct)
+        win.addstr(0, int(border[1]/2)-int(len(title)/2), title, curses.A_BOLD)
+        speed, body, apple, pois, points = food(q, head, apple, points, body, speed, pois, poison_ed, win, border)
+        head = forward(head, headC, direct, speed, q)
+        body = bodygrow(body, head, win)
+        win.addstr(head[0], head[1], headC, curses.color_pair(2))
+        win.refresh()
+        q = win.getch()
+        poison_ed, pois = poison(q, head, apple, points, body, speed, pois, poison_ed, win, border)
+        q = hitWall(win, head, border, q)
+    poison_ed, q, headC = gameover(poison_ed, q, headC, border, points, win)
 
 
 game()
